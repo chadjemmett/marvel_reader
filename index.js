a0b60cb3a837c22174748a56dfa1fd1a7a649d7d
Chad Jemmett <chad.jemmett@gmail.com>
2019-07-19 11:10:19 -0600

    Trying to combine the paragraph data

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const db = knex(knexConfig.development);
 // const w = new Wikia({wiki: "marvel"});
 
 
-
 const id = 121;
 
 axios.get('https://marvel.fandom.com/api/v1/Articles/Details?ids=50&titles=Amazing_Spider-Man_Vol_1_ ' + id + '&abstract=100&width=200&height=200')
@@ -18,7 +17,8 @@ axios.get('https://marvel.fandom.com/api/v1/Articles/Details?ids=50&titles=Amazi
     .then(issue_data => {
       // console.log('issue data', issue_data);
       const new_string = issue_data.data.sections[2]['content'].map(item => {
-        console.log(item, '\n');
+        console.log(item.text, '\n');
+
       })
      })//console.log(issue_data.data.sections[2]['content']))
   .catch(err => console.log(err));
